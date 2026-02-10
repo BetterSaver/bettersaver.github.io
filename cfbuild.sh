1414f2c6cf610f7e7d699d246ee1c09b022fd63b
William Tonkin-Howe <william.t.howe@gmail.com>
2026-02-10 21:48:18 +1300

    fix url rewrite for staging

--- cfbuild.sh
+++ cfbuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# open config.toml and set baseURL to https://bettersaver-staging.zac-40e.workers.dev/
+# update links to staging site in content files
+sed -i -E "s|http://localhost:4200|https://bettersaver-dev.web.app|g" config.toml
 
+# open config.toml and set baseURL to https://bettersaver-staging.zac-40e.workers.dev/
 sed -i -E "s|^baseURL\s*=\s*['\"][^'\"]+['\"]|baseURL = 'https://bettersaver-staging.zac-40e.workers.dev/'|" config.toml
 
-# update links to staging site in content files
-find . -type f -name "*.md" -exec sed -i 's|http://localhost:4200|https://bettersaver-dev.web.app|g' {} +
