3d1c32053804faa6e271561ca3ef0f205de3d455
William Tonkin-Howe <william.t.howe@gmail.com>
2026-02-19 03:13:42 +0000

    update to use staging -app url

--- cfbuild.sh
+++ cfbuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # update links to staging site in content files
-sed -i -E "s|http://localhost:4200|https://bettersaver-dev.web.app|g" config.toml
-sed -i -E "s|http://app.bettersaver.co.nz|https://bettersaver-dev.web.app|g" config.toml
+sed -i -E "s|http://localhost:4200|https://staging-app.bettersaver.co.nz/|g" config.toml
+sed -i -E "s|http://app.bettersaver.co.nz|https://staging-app.bettersaver.co.nz/|g" config.toml
 
 # open config.toml and set baseURL to https://bettersaver-staging.zac-40e.workers.dev/
 sed -i -E "s|^baseURL\s*=\s*['\"][^'\"]+['\"]|baseURL = 'https://bettersaver-staging.zac-40e.workers.dev/'|" config.toml
