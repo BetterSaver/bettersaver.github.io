db61a5e19eeff14f57aa5577eec3e6fdce075289
William Tonkin-Howe <william.t.howe@gmail.com>
2026-06-03 20:04:44 +1200

    rating pop up and hanged the port the site runs on locally to match Pauls changes

--- cfbuild.sh
+++ cfbuild.sh
@@ -2,6 +2,7 @@
 
 # update links to staging site in content files
 sed -i -E "s|http://localhost:4200|https://staging-app.bettersaver.co.nz|g" config.toml
+sed -i -E "s|http://localhost:4250|https://staging-app.bettersaver.co.nz|g" config.toml
 sed -i -E "s|http://app.bettersaver.co.nz|https://staging-app.bettersaver.co.nz|g" config.toml
 
 # open config.toml and set baseURL to https://bettersaver-staging.zac-40e.workers.dev/ - fixes images not loading on staging site
